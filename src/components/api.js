0be607319ee387ca69080dc405178f57f3f60423
skynation0424 <skynation0424@gmail.com>
2024-10-06 15:19:51 +0900

    init

---
+++ src/components/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+ 
+const BASE_URL = 'http://localhost:8080/api'; // 백엔드 서버 주소에 맞게 수정
+ 
+export const fetchRecommendedCategory = async (userId) => {
+  const response = await axios.get(`${BASE_URL}/recommendations/category?userId=${userId}`);
+  return response.data;
+};
+ 
+export const fetchTopSpendingCategory = async (userId) => {
+  const response = await axios.get(`${BASE_URL}/spending/top-category?userId=${userId}`);
+  return response.data;
+};
+ 
+export const fetchSubscriptionsByCategory = async (category) => {
+  const response = await axios.get(`${BASE_URL}/subscriptions/by-category?category=${category}`);
+  return response.data;
+};
+ 
+export const fetchSubscriptionDetails = async (subscriptionId) => {
+  const response = await axios.get(`${BASE_URL}/subscriptions/${subscriptionId}`);
+  return response.data;
+};
