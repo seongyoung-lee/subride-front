17683fbc274b0831ee473707e9515d292b4278d0
seongyoung=lee <leeseongyoung88@gmail.com>
2024-10-21 16:19:39 +0900

    add cicd

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
  
-const BASE_URL = 'http://localhost:8080/api'; // 백엔드 서버 주소에 맞게 수정
+//const BASE_URL = 'http://localhost:8080/api'; // 백엔드 서버 주소에 맞게 수정
+//const BASE_URL = 'http://localhost:19080/api/subrecommend'; // 백엔드 서버 주소에 맞게 수정
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:19080";
+const BASE_URL = `${API_BASE_URL}/api/subrecommend`; // 백엔드 서버 주소에 맞게 수정
  
 export const fetchRecommendedCategory = async (userId) => {
   const response = await axios.get(`${BASE_URL}/recommendations/category?userId=${userId}`);
